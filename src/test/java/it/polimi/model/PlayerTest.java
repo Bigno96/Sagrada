0b108dec7c4668c69c9d96c2d27735e6a8dc1b7a
Bigno96 <andrea.bignoli96@gmail.com>
2018-05-04 23:49:12 +0200

    Added working WindowFactory, updated UML

--- src/test/java/it/polimi/model/PlayerTest.java
+++ src/test/java/it/polimi/model/PlayerTest.java
@@ -2,30 +2,18 @@ package it.polimi.model;
 
 import junit.framework.TestCase;
 
-<<<<<<< HEAD
-import java.util.ArrayList;
 import java.util.List;
-=======
-import static org.junit.jupiter.api.Assertions.assertEquals;
->>>>>>> e5fe01a817e2327149cd57a141a9c97860736a96
 
 public class PlayerTest extends TestCase {
 
     private Player player1 = new Player(1);
     private Player player2 = new Player(2);
+    private WindowFactory winFact = new WindowFactory();
 
     public PlayerTest(String testName){
         super(testName);
     }
 
-<<<<<<< HEAD
-    public void testGetId() {
-        assertEquals(1, player1.getID());
-        assertEquals(2, player2.getID());
-    }
-
-    public void testFavorPoint() {
-=======
     public void testGetId(){
         assertEquals(1, player1.getId());
         assertEquals(2, player2.getId());
@@ -36,9 +24,18 @@ public class PlayerTest extends TestCase {
     }
 
     public void testSetFavor2(){
->>>>>>> e5fe01a817e2327149cd57a141a9c97860736a96
         player1.setFavorPoint(1);
         assertEquals(1, player1.getFavorPoint());
+    }
+
+    public void testWindCard() {
+        int x = 2;
+        int y = 4;
+        List<WindowCard> winCardList = winFact.getWindow(x,y);
+        WindowCard windCard =  winCardList.get(0);
+        assertSame(null, player1.getWind());
+        player1.setWind(windCard);
+        assertSame(windCard, player1.getWind());
     }
 
 }
