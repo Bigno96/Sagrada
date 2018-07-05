3cf03556ff15116feb9ee86d1d0f275ed6bae628
AlaGio <galampi@hotmail.it>
2018-07-05 14:17:41 +0200

    Fix

--- src/main/java/it/polimi/ingsw/client/view/gui/BoardController.java
+++ src/main/java/it/polimi/ingsw/client/view/gui/BoardController.java
@@ -13,8 +13,6 @@ import javafx.scene.image.ImageView;
 
 import java.util.List;
 
-import static java.lang.System.out;
-
 public class BoardController implements ControlInterface {
 
     @FXML
@@ -29,8 +27,6 @@ public class BoardController implements ControlInterface {
     public TextArea textArea;
 
     private GuiSystem guiSystem;
-
-    private WindowCard windowCard;
 
     @Override
     public void print(String s) {
@@ -51,6 +47,8 @@ public class BoardController implements ControlInterface {
     @Override
     public void newCard() {
 
+        //tabella sulla carta
+
         String baseURL = "/img/WindowCard/";
         String exp = ".png";
 
@@ -66,14 +64,23 @@ public class BoardController implements ControlInterface {
 
         if(guiSystem.windowCards.size() == 2){
 
-            Image thirdWindow = new Image(baseURL + guiSystem.windowCards.get(0).getName() + exp);
+            Image secondWindow = new Image(baseURL + guiSystem.windowCards.get(0).getName() + exp);
+            img1.setImage(secondWindow);
+
+            Image thirdWindow = new Image(baseURL + guiSystem.windowCards.get(1).getName() + exp);
             img2.setImage(thirdWindow);
 
         }
 
         if(guiSystem.windowCards.size() == 3){
 
-            Image fourthWindow = new Image(baseURL + guiSystem.windowCards.get(0).getName() + exp);
+            Image secondWindow = new Image(baseURL + guiSystem.windowCards.get(0).getName() + exp);
+            img1.setImage(secondWindow);
+
+            Image thirdWindow = new Image(baseURL + guiSystem.windowCards.get(1).getName() + exp);
+            img2.setImage(thirdWindow);
+
+            Image fourthWindow = new Image(baseURL + guiSystem.windowCards.get(2).getName() + exp);
             img3.setImage(fourthWindow);
 
         }
@@ -83,30 +90,48 @@ public class BoardController implements ControlInterface {
     @Override
     public void printDraft(List<Dice> Draft) {
 
+        //print Dice
+
     }
 
     @Override
     public void printPrivateObj(ObjectiveCard privObj) {
+
+        //print..
 
     }
 
     @Override
     public void printListToolCard(List<ToolCard> toolCards) {
 
+        //print...
+
     }
 
     @Override
     public void printListPublObj(List<ObjectiveCard> publObj) {
+
+        //print...
 
     }
 
     @Override
     public void updateCard(WindowCard window) {
 
+        if(window.getName().equals(guiSystem.getMyWindowCard().getName())){
+
+        }else{
+            //ciclo per cercare la carta
+            //ciclo per inserire i dadi
+
+        }
+
     }
 
     @Override
     public void updateRoundTrack(RoundTrack roundTrack) {
+
+        //print..
 
     }
 
